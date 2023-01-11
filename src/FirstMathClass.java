583ab19e57db8c57d85d6be54e8bdbc7d8c2d962
ushasatyam <ushasatyam@gmail.com>
2023-01-10 23:52:55 -0600

    initial commition 10 Jan 2023

---
+++ src/FirstMathClass.java
@@ -0,0 +1,128 @@
+/**
+ * 
+ */
+
+/**
+ * @author ushas
+ *
+ */
+public class FirstMathClass 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		
+		int age;
+		
+		age = 15;
+		
+		System.out.println( age);
+		
+		age = 16;
+		
+		System.out.println(age);
+		
+		age = 17;
+		
+		System.out.println(age);
+		
+		System.out.println("Arshu");
+		
+		System.out.println("Chikku");
+		
+		
+		System.out.println("Usha");
+		
+		
+		String colour;
+		
+		colour = "green";
+		
+		System.out.println(colour);
+		
+		colour = "white";
+		
+		System.out.println(colour);
+		
+		System.out.println(age);
+		
+		age = 32;
+		
+		System.out.println(age);
+		
+		colour = "red";
+		
+		System.out.println(colour);
+		
+		colour =" yellow";
+		
+		System.out.println( colour);
+		
+		colour =" blue";
+		
+		System.out.println(colour);
+		
+		
+		colour = "orange";
+		
+		System.out.println(colour);
+		
+		colour = "purple";
+		
+		System.out.println(colour);
+		
+		
+		colour ="brown";
+		
+		System.out.println(colour);
+		
+		colour = "black";
+		
+		System.out.println(colour);
+		
+		age =  89;
+		
+		System.out.println(age);
+		
+		age = 45;
+		
+		System.out.println(age);
+		
+		
+		System.out.println("God is great");
+		
+		System.out.println("God loves everyone");
+		
+		age = 10 ;
+		
+		System.out.println(age);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+	
+		
+		
+		
+		
+		
+	}
+
+}
